99d0f73a6f83cde3186824e358e6f42f63179f36
trihx.dtp <trihx.dtp@vnpt.vn>
2023-03-26 09:27:45 +0700

    Sua lai file app.js thanh Portfolio

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const http = require('http');
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Hello World</h1>');
+  res.end(
+    '<h1>MY PORTFOLIO</h1><h4>Hello, I am Xuan-Tri, a software developer living in SaDec - Dong Thap with more than 10 years of experience.</h4><p>I love the process as much as a outcome, I take a creative, collaborative and strategic approach to defining and designing solutions</p>'
+  );
 });
 
-server.listen(port,() => {
-  console.log(`Server running at port `+port);
+server.listen(port, () => {
+  console.log(`Server running at port ` + port);
 });
-
